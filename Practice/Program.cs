653022efedf37ba4144f62cbcbb0f53e2e72789e
Justin Vocke <therealmathhelper@gmail.com>
2024-04-22 15:28:47 -0500

    Add missing files

---
+++ Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+public class Solution1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var num1 = 0;
+        var num2 = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    num1 = i;
+                    num2 = j;
+                    break;
+                }
+                
+            }
+        }
+        return [num1, num2];
+    }
+}
+
+
